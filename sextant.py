5018188658191fcd5aa4d846f7afc095a53d2856
Derek Homeier <dhomeie@gwdg.de>
2023-05-20 14:44:14 +0200

    Eingabe in Ortszeit; Zeitdifferenz in Minuten

--- sextant.py
+++ sextant.py
@@ -8,14 +8,17 @@ import pyodide_http
 import pyodide
 pyodide_http.patch_all()
 
+import warnings
+import numpy as np
+
 from astropy import coordinates
+from astropy import units as u
 from astropy.time import Time
 from astropy.coordinates.name_resolve import NameResolveError
 from astropy.utils.exceptions import AstropyWarning
 from astropy.utils.iers.iers import IERSWarning
 from erfa.core import ErfaWarning
 
-import warnings
 
 def populate_dropdowns(elem):
     # Hardcode some star names as connecting to CDS seems problematic under PyScript
@@ -35,51 +38,40 @@ latin = Element("geolat")
 starin = Element("starpos")
 mout = Element("mooncoo")
 mdis = Element("moondis")
+utime = Element("utctime")
+tdiff = Element("timediff")
 ebutton = Element("ephembutton")
 dbutton = Element("distbutton")
+tbutton = Element("utcbutton")
 refstar = Element("refstar")
 starcoo = Element("starcoo")
+stardis = Element("stardis")
 
 populate_dropdowns(refstar.element)
 
 ebutton.element.disabled = False
 dbutton.element.disabled = False
+tbutton.element.disabled = False
+
+loctime = '1761-01-15 19:55:00'
 
 
-def ephem():
+def get_obsloc(lon=None, lat=None):
+    if lon is None:
+        lon = lonin.element.value
+    if lat is None:
+        lat = latin.element.value
     try:
-        obsloc = coordinates.EarthLocation.from_geodetic(lonin.element.value, latin.element.value)
+        obsloc = coordinates.EarthLocation.from_geodetic(lon, lat)
     except Exception as e:
-        mout.write(f"Fehler bei Positionseingabe: {e} -\t"
-                   f"Länge, Breite: {lonin.element.value} {latin.element.value}")
+        mout.write(f"Fehler bei Positionseingabe zu Länge, Breite {lon}, {lat}: {e}")
+        return None
     else:
         mout.write(f"Koordinaten: {obsloc.lon}, {obsloc.lat}")
+        return obsloc
 
-    with warnings.catch_warnings():
-        # Filter warnings about dates out of range (ERFA outside of range 1900-2100)
-        warnings.simplefilter("ignore", category=ErfaWarning)
-        try:
-            obstime = Time(f"{datein.element.value} {timein.element.value}")
-        except Exception as e:
-            mout.write(f"Fehler bei Datumseingabe: {e} \n{datein.element.value} {timein.element.value}")
-        else:
-            mout.write(f"Datum, Zeit: {datein.element.value} {timein.element.value} {obstime}")
 
-        try:
-            with warnings.catch_warnings():
-                warnings.filterwarnings("ignore", category=IERSWarning)
-                warnings.filterwarnings("ignore", message=".*IERS [dt]a.*", category=AstropyWarning)
-                mooncoo = coordinates.get_moon(obstime, obsloc, ephemeris='builtin')
-        except Exception as e:
-            mout.write(f"Fehler in Mondposition: {e} - Datum, Position: "
-                       f"{datein.element.value} {timein.element.value}; {lonin} {latin}")
-        else:
-            mout.write(f"Mondposition von {obsloc.lon:.3f} Länge {obsloc.lat:+.3f} Breite am "
-                   f"{obstime} (UTC): {mooncoo.to_string('hmsdms', precision=2)}")
-
-    return mooncoo
-
-def distanz(mooncoo):
+def get_starcoo():
     if len(starin.element.value.strip()) > 2:
         starcoo = None
         try:
@@ -98,7 +90,40 @@ def distanz(mooncoo):
             mdis.write(f"Sternposition {starcoo.to_string('hmsdms', precision=2)}")
     else:
         starcoo = coordinates.SkyCoord(refstar.element.value)
+    return starcoo
 
+
+def ephem():
+    obsloc = get_obsloc()
+    if obsloc is None:
+        return None
+    with warnings.catch_warnings():
+        # Filter warnings about dates out of range (ERFA outside of range 1900-2100)
+        warnings.simplefilter("ignore", category=ErfaWarning)
+        try:
+            obstime = Time(f"{datein.element.value} {timein.element.value}") - obsloc.lon.deg * 4 * u.min
+        except Exception as e:
+            mout.write(f"Fehler bei Datumseingabe für {datein.element.value} {timein.element.value} {e}")
+            return None
+        else:
+            mout.write(f"Datum, Zeit: {datein.element.value} {timein.element.value} {obstime}")
+
+        try:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", category=IERSWarning)
+                warnings.filterwarnings("ignore", message=".*IERS [dt]a.*", category=AstropyWarning)
+                mooncoo = coordinates.get_moon(obstime, obsloc, ephemeris='builtin')
+        except Exception as e:
+            mout.write(f"Fehler in Mondposition: {e} - Datum, Position: "
+                       f"{datein.element.value} {timein.element.value}; {lonin} {latin}")
+        else:
+            mout.write(f"Mondposition von {obsloc.lon:.3f} Länge {obsloc.lat:+.3f} Breite am "
+                   f"{obstime} (UTC): {mooncoo.to_string('hmsdms', precision=2)}")
+    return mooncoo
+
+
+def distanz(mooncoo):
+    starcoo = get_starcoo()
     if starcoo is not None:
         try:
             with warnings.catch_warnings():
@@ -108,3 +133,33 @@ def distanz(mooncoo):
                        f"    {moondis.to_string(precision=1)}")
         except Exception as e:
             mdis.write(f"Fehler bei Distanzbestimmung zu {starcoo.to_string('hmsdms')}: {e}")
+
+
+def utctime():
+    try:
+        angle = stardis.element.value.lower()
+        if 'd' in angle or 'm' in angle or 's' in angle:
+            dunit = None
+        else:
+            dunit = 'deg'
+        mydist = coordinates.Angle(angle, unit=dunit)
+    except Exception as e:
+        utime.write(f"Fehler beim Lesen von Winkelabstand {stardis.element.value}: {e}")
+        return
+    starcoo = get_starcoo()
+    obsloc = get_obsloc(lon=-3.75)  # Setze Länge auf Inputwert 3d45m W
+    if starcoo is not None and obsloc is not None:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=ErfaWarning)
+            warnings.filterwarnings("ignore", category=IERSWarning)
+            warnings.filterwarnings("ignore", message=".*IERS [dt]a.*", category=AstropyWarning)
+
+            obstimes = Time('1761-01-15 16:00', scale='utc') + range(16 * 60) * u.minute
+            mooncoords = coordinates.get_moon(obstimes, obsloc, ephemeris='builtin')
+            distances = mooncoords.separation(starcoo)
+            idx = np.argmin(abs(distances - mydist))
+            utime.write(f"Beste Monddistanz zu {starcoo.to_string('hmsdms', precision=0)} ist "
+                        f"{distances[idx].to_string(precision=0)} um {obstimes[idx]} "
+                        "(Greenwich Mean Time)")
+            tdiff.write(f"Differenz Weltzeit – Ortszeit = "
+                        f"{(obstimes[idx]-Time(loctime)).to_value('min'):.2f} Minuten")
